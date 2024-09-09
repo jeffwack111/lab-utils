773fcdab3f56fd201cbdf491596f7ae69e923e4a
Jeffrey Wack <jeffwack111@gmail.com>
2024-09-09 11:56:22 -0700

    organization

--- pyro5/check.py
+++
@@ -1,6 +0,0 @@
-import Pyro5.client
-from moku.instruments import MultiInstrument, LaserLockBox, DigitalFilterBox, FrequencyResponseAnalyzer, SpectrumAnalyzer
-
-llb = Pyro5.client.Proxy("PYRONAME:llb3")
-
-print(llb.summary()) 
